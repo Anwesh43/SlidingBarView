073f3e4b531e16244eb426820cf72da6d54c681d
Anwesh43 <anweshthecool0@gmail.com>
2020-03-26 02:03:56 +0530

    adding SBBNode class

--- slidingbouncybarview/src/main/java/com/anwesh/uiprojects/slidingbouncybarview/SlidingBouncyBarView.kt
+++ slidingbouncybarview/src/main/java/com/anwesh/uiprojects/slidingbouncybarview/SlidingBouncyBarView.kt
@@ -116,4 +116,46 @@ class SlidingBouncyBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBBNode(var i : Int, val state : State = State()) {
+
+        private var next : SBBNode? = null
+        private var prev : SBBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SBBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBBNode {
+            var curr : SBBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
