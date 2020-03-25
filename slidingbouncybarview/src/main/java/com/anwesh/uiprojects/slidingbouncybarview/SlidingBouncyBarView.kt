30b8c37b499011ae6e40082096f8be6e8a6c1296
Anwesh43 <anweshthecool0@gmail.com>
2020-03-26 02:35:21 +0530

    adding renderer in view

--- slidingbouncybarview/src/main/java/com/anwesh/uiprojects/slidingbouncybarview/SlidingBouncyBarView.kt
+++ slidingbouncybarview/src/main/java/com/anwesh/uiprojects/slidingbouncybarview/SlidingBouncyBarView.kt
@@ -56,14 +56,16 @@ fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
 
 class SlidingBouncyBarView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -187,8 +189,9 @@ class SlidingBouncyBarView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val sbb : SlidingBouncyBar = SlidingBouncyBar(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             sbb.draw(canvas, paint)
             animator.animate {
