4c6d601005d90394e32e60fa59266e605ded670c
Anwesh43 <anweshthecool0@gmail.com>
2020-03-26 02:39:19 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedslidingbouncybarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedslidingbouncybarview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedslidingbouncybarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.slidingbouncybarview.SlidingBouncyBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SlidingBouncyBarView.create(this)
     }
 }
