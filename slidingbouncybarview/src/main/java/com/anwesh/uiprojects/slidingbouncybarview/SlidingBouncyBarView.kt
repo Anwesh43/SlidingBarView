1bbdde120127f15388c20eac1d3a179d98983e1f
Anwesh43 <anweshthecool0@gmail.com>
2020-03-26 01:38:39 +0530

    adding constants and libraries

--- slidingbouncybarview/src/main/java/com/anwesh/uiprojects/slidingbouncybarview/SlidingBouncyBarView.kt
+++ slidingbouncybarview/src/main/java/com/anwesh/uiprojects/slidingbouncybarview/SlidingBouncyBarView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.slidingbouncybarview
 /**
  * Created by anweshmishra on 26/03/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val sizeFactor: Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
